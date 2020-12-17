b912997ea6c12dae7178c1db06b6f11e5443c278
Sideswipe47 <sideswipe47@gmail.com>
2020-12-18 01:26:41 +0700

    Add

--- database/seeds/DeliveryOptionSeeder.php
+++ database/seeds/DeliveryOptionSeeder.php
@@ -17,11 +17,11 @@ class DeliveryOptionSeeder extends Seeder
         $delivery_option_data = [
             ['delivery_service_id' => 1, 'description' => 'Same Day Delivery', 'cost' => 20000, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
             ['delivery_service_id' => 1, 'description' => 'Regular Day (1 - 3 days)', 'cost' => 10000, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['delivery_service_id' => 1, 'description' => 'Free Delivery (3 - 7 days)', 'cost' => 0],
-            ['delivery_service_id' => 2, 'description' => 'Regular Day (1 - 5 days)', 'cost' => 5000],
-            ['delivery_service_id' => 2, 'description' => 'Free Delivery (5 - 7 days)', 'cost' => 0],
-            ['delivery_service_id' => 3, 'description' => 'Same Day Delivery', 'cost' => 15000],
-            ['delivery_service_id' => 3, 'description' => 'Regular Day (1 - 3 days)', 'cost' => 7500],
+            ['delivery_service_id' => 1, 'description' => 'Free Delivery (3 - 7 days)', 'cost' => 0, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['delivery_service_id' => 2, 'description' => 'Regular Day (1 - 5 days)', 'cost' => 5000, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['delivery_service_id' => 2, 'description' => 'Free Delivery (5 - 7 days)', 'cost' => 0, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['delivery_service_id' => 3, 'description' => 'Same Day Delivery', 'cost' => 15000, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['delivery_service_id' => 3, 'description' => 'Regular Day (1 - 3 days)', 'cost' => 7500, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
         ];
         
         DB::table('delivery_options')->insert($delivery_option_data);
